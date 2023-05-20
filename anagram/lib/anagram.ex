7357f53cd28feee70866ab949b29b09aab64c5dd
Nicolai Hedegaard Jensen <nicolaihedegaardjensen@Nicolais-MacBook-Pro.local>
2023-05-20 20:55:18 +0200

    solved anagram

---
+++ anagram/lib/anagram.ex
@@ -0,0 +1,14 @@
+defmodule Anagram do
+  def match(base, candidates) do
+    candidates
+    |> Enum.filter(fn candidate -> String.downcase(base) != String.downcase(candidate) end)
+    |> Enum.uniq_by(fn candidate -> String.downcase(candidate) end)
+    |> Enum.filter(fn candidate -> check_letter_match(base, candidate) end)
+  end
+
+  defp check_letter_match(base, candidate) do
+    sorted_base = String.downcase(base) |> String.codepoints() |> Enum.sort()
+    sorted_candidate = String.downcase(candidate) |> String.codepoints() |> Enum.sort()
+    Enum.join(sorted_base) == Enum.join(sorted_candidate)
+  end
+end
